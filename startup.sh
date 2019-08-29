268cbc1da4580ba52cd9fb07ceec874c3daba4e8
ysbre <54414789+ysbre@users.noreply.github.com>
2019-08-29 15:19:47 -0700

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,6 @@
+systemctl start devopsdash-api.service
+systemctl start devopsdash-ui.service
+systemctl start collector-bitbucket.service
+systemctl start collector-jira.service
+systemctl start collector-score.service
+systemctl start collector-sonar.service
